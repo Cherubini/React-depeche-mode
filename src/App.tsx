1e3d4477dc3c7c4e81f6550c706f66720dd4d001
Cherubini <githubvalentinacherubini@gmail.com>
2023-06-15 14:26:45 +0200

    created folder assets and modified file app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,22 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <p>Init</p>
+       <header className="header">
+        <h1 className="title">Depeche Mode</h1>
+        <ul className="nav-list">
+          <li className="nav">Home</li>
+          <li className="nav">Biography</li>
+          <li className="nav">Albums</li>
+          <li className="nav">Gallery</li>
+        </ul>
+      </header>
+        {/* <Outlet /> */}
+      <footer className="footer">
+        <address>
+          Davide Consigliere, Valentina Cherubini<br/>
+          AlgoWatt, Genova(GE), 2023
+        </address>
+      </footer>
     </div>
   );
 }
