c839b6dbe77e0e6df435fb6d0e7e8a686ae5806d
dave89dc <dave89dc@gmail.com>
2023-06-16 12:29:38 +0200

    change style.scss to style.module.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import classes from './App.module.scss';
+import classes from './index.module.scss';
 
 function App(): JSX.Element {
 
